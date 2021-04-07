7cfcabc98853efe187c7030e9b702e0813cee451
manne65-hd <manne@hoffmaenner.net>
2021-04-08 01:17:04 +0200

    Update german translations

--- Locale/de_DE/translations.php
+++ Locale/de_DE/translations.php
@@ -1,5 +1,5 @@
 <?php
 
 return array(
-    'Delegate a task to this project' => 'Delegiere eine Aufgabe an dieses Projekt',
+    'Delegate a task to this project' => 'Delegieren Sie eine Aufgabe an dieses Projekt',
 );
---
+++ Locale/de_DE_du/translations.php
@@ -0,0 +1,5 @@
+<?php
+
+return array(
+    'Delegate a task to this project' => 'Delegiere eine Aufgabe an dieses Projekt',
+);
